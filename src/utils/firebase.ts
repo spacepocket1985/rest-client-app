2f15965fdd52dc5cf47dc1256a007dc8f41e7edd
ab3MN <ab3mn3@gmail.com>
2025-03-28 08:24:35 +0200

    refactor:remove getEnvVar

--- src/utils/firebase.ts
+++ src/utils/firebase.ts
@@ -2,16 +2,15 @@ import { FirebaseOptions, initializeApp } from 'firebase/app';
 import { getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword, signOut, User } from 'firebase/auth';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import { getFirestore, collection, addDoc, getDocs, where, query, DocumentData } from 'firebase/firestore';
-import { getEnvVar } from './getEnvVar';
 
 const firebaseConfig: FirebaseOptions = {
-  apiKey: getEnvVar('NEXT_PUBLIC_FIREBASE_API_KEY'),
-  authDomain: getEnvVar('NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN'),
-  projectId: getEnvVar('NEXT_PUBLIC_FIREBASE_PROJECT_ID'),
-  storageBucket: getEnvVar('NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET'),
-  messagingSenderId: getEnvVar('NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID'),
-  appId: getEnvVar('NEXT_PUBLIC_FIREBASE_APP_ID'),
-  measurementId: getEnvVar('NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID'),
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY || '',
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN || '',
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID || '',
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET || '',
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID || '',
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID || '',
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID || '',
 };
 
 const app = initializeApp(firebaseConfig);
--- src/utils/getEnvVar.ts
+++
@@ -1,7 +0,0 @@
-export const getEnvVar = (key: string): string => {
-  const value = process.env[key];
-
-  if (!value) throw new Error(`Missing env variable: ${key}`);
-
-  return value;
-};
