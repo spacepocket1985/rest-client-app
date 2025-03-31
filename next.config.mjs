ad7bd0579edbf502e2067751f9355b9a19a3a165
ordinaraviro <ordinaraviro@gmail.com>
2025-03-31 12:49:17 +0300

    feat: add and setup i18n

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,7 @@
 // next.config.js
+import createNextIntlPlugin from 'next-intl/plugin';
+
+const withNextIntl = createNextIntlPlugin();
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
@@ -18,4 +21,5 @@ const nextConfig = {
   },
 };
 
-export default nextConfig;
+// export default nextConfig;
+export default withNextIntl(nextConfig);
---
+++ src/app/[locale]/page.tsx
@@ -0,0 +1,13 @@
+import { useTranslations } from 'next-intl';
+
+export default function WelcomePage() {
+  const t = useTranslations('WelcomePage');
+  return (
+    <>
+      <div className={'welcomeWrapper'}>
+        <h2>{t('title')}</h2>
+        <h4>{t('welcome')}</h4>
+      </div>
+    </>
+  );
+}
