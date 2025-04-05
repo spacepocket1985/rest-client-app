b392cfe8b52f02a9098870ed3ca5cbed534f57d1
Space Pocket <space1985pocket@gmail.com>
2025-04-05 18:00:31 +0300

    refactor: update UIFormInput

--- src/ui/UIFormInput.tsx
+++ src/ui/UIFormInput.tsx
@@ -56,7 +56,6 @@ export const UIFormInput = <T extends FieldValues>({
           type="button"
           onClick={togglePasswordVisibility}
           className="absolute right-3 top-6 transform -translate-y-1/2 text-gray-500 hover:text-gray-700 cursor-pointer"
-          aria-label={showPassword ? t('hidePassword') : t('showPassword')}
         >
           {showPassword ?
             <EyeIcon />
