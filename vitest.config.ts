cea0d89bdc7a12f066e3c8ab596b6430af54b800
Space Pocket <space1985pocket@gmail.com>
2025-04-17 00:05:23 +0300

    refactor: add FormSignIn test

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,6 +1,9 @@
 import { defineConfig } from 'vitest/config';
 import tsconfigPaths from 'vite-tsconfig-paths';
 import react from '@vitejs/plugin-react';
+import dotenv from 'dotenv';
+
+dotenv.config({ path: '.env.test' });
 
 export default defineConfig({
   plugins: [tsconfigPaths(), react()],
