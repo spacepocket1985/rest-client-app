d90f8e3a9c7e32f49303fe908d550acd235bc936
Space Pocket <space1985pocket@gmail.com>
2025-04-20 19:04:16 +0300

    refactor:update lazy routes

--- src/app/[locale]/history/page.tsx
+++ src/app/[locale]/history/page.tsx
@@ -1,5 +1,13 @@
-import History from '@components/history/History';
+'use client';
+
+import dynamic from 'next/dynamic';
+import { Spinner } from '@components/spinner/Spinner';
+
+const ProtectedHistory = dynamic(() => import('@components/history/History'), {
+  loading: () => <Spinner />,
+  ssr: false,
+});
 
 export default function HistoryPage() {
-  return <History />;
+  return <ProtectedHistory />;
 }
